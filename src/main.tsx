861030ba5fa64527000cf449651808aba75a4cf8
sean <sean@gometa.io>
2025-09-05 13:48:18 -0400

    sc fix for prerender compilation

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import GlobalStyle from './style/GlobalStyle.tsx';
+import { StyleSheetManager } from 'styled-components';
+
+const isCrawler = () =>
+  typeof navigator !== 'undefined' &&
+  (navigator.userAgent.includes('Prerender') ||
+    navigator.userAgent.includes('prerender'));
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GlobalStyle />
-    <App />
+    <StyleSheetManager disableCSSOMInjection={isCrawler()}>
+      <GlobalStyle />
+      <App />
+    </StyleSheetManager>
   </StrictMode>,
 );

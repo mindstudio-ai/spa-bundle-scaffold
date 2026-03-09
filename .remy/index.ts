2b081fab24012e7cbdefcf1b2612b7855077182f
sean <sean@gometa.io>
2026-03-09 11:01:03 -0400

    Update index.ts

--- .remy/index.ts
+++ .remy/index.ts
@@ -239,9 +239,10 @@ const spawnDevServer = () => {
   });
 
   child.stderr.on('data', (chunk: string) => {
-    onLog(chunk);
-    if (chunk.includes('ready in')) {
+    onLog(chunk, `${Date.now()}`);
+    if (chunk.includes('ready')) {
       onLog(`ready ${Date.now()}`);
+      console.log('@@remy:ready@@'); // Sentinel for sandbox host
     }
   });
 
